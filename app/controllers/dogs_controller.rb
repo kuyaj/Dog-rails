4efa60b9b73f09b316343ee039a80365c6813035
Angelo <valmocinajayangelo@gmail.com>
2019-08-16 18:49:16 +0800

    Modify frontend ui

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,7 +2,7 @@ class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
 
   def index
-    @dogs = Dog.all
+    @dogs = Dog.all.order("created_at DESC")
   end
 
   def show
