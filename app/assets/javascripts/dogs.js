4b8bc7f86d9ac8bf84dea8120b4a4524668d0150
unknown <valmocinajayangelo@gmail.com>
2019-08-18 02:50:46 +0800

    Modify dog.js; Added turbolinks:load

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,18 +1,14 @@
-window.addEventListener('click', function() {
+document.addEventListener('turbolinks:load', function(){
   var upload_image = document.getElementById('dog_avatar');
   var segment_compact = document.getElementById("segment-preview-image");
-
+  
   upload_image.addEventListener('click', function() {
-
     this.addEventListener('change', () => {
-
-      if (this.files && this.files[0]) {
-        var img = document.querySelector('img');  // $('img')[0]      
-        img.src = URL.createObjectURL(this.files[0]); // set src to file url
+      if (this.files && this.files[0]) {     
+        var image = document.querySelector('img');  // $('img')[0] 
+        image.src = URL.createObjectURL(this.files[0]); // set src to file url
       }
-
-          segment_compact.removeAttribute("hidden");
+      segment_compact.removeAttribute("hidden");
      });
   })
-})
-
+});
