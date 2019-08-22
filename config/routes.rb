bdfdf8f03cf13bb525ab02a84f5d79db1efe6e19
unknown <valmocinajayangelo@gmail.com>
2019-08-22 17:55:46 +0800

    Added scss for login and register forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'sessions/login'
+  get 'sessions/register'
   root 'dogs#index'
   resources :dogs
-  
+
   # get 'dogs/index'
   # get 'dogs/show'
   # get 'dogs/new'
   # get 'dogs/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_132838) do
+ActiveRecord::Schema.define(version: 2019_08_22_084143) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2019_08_08_132838) do
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.string "motto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
